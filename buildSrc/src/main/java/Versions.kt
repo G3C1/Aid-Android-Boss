53ef11d07576ea679a94a0824b3fb9e627e12774
khs3994 <z01039948003@gmail.com>
2022-11-16 10:24:09 +0900

    :sparkles: :: 버전 관리

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,9 +3,22 @@ object Versions {
     const val MinSdk = 24
     const val TargetSdk = 32
 
+    const val APP_COMPAT = "1.4.1"
+    const val CORE_KTX = "1.7.0"
+    const val LIFECYCLE = "2.3.1"
+
+    const val JUNIT = "4.13.2"
+    const val ANDROID_JUNIT = "1.1.3"
+    const val ESPRESSO = "3.4.0"
+
+    const val ACTIVITY_COMPOSE = "1.3.1"
     const val COMPOSE = "1.2.0-alpha03"
+    const val COMPOSE_NAV = "2.5.1"
+
     const val HILT = "2.38.1"
 
     const val RETROFIT = "2.7.1"
     const val OKHTTP = "3.14.9"
+
+    const val SYSTEM_UI_CONTROLLER = "0.23.1"
 }
