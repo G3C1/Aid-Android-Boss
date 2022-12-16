14c79e0786aad7d4417749f0e967d1bb87e77717
khs3994 <z01039948003@gmail.com>
2022-12-16 14:42:05 +0900

    :memo: :: Interceptor 수정

--- app/src/main/java/com/g3c1/aide/remote/utils/TokenInterceptor.kt
+++ app/src/main/java/com/g3c1/aide/remote/utils/TokenInterceptor.kt
@@ -1,16 +1,22 @@
 package com.g3c1.aide.remote.utils
 
 import com.g3c1.aide.di.AideBossApplication
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 
 class TokenInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response = with(chain) {
-        val tokenRequest =
-            request().newBuilder().addHeader(
-                "Authorization",
-                AideBossApplication.getInstance().getTokenManager().accessToken.toString()
-            ).build()
-        return proceed(tokenRequest)
+        lateinit var accessToken: Request
+        runBlocking {
+            accessToken =
+                request().newBuilder().addHeader(
+                    "Authorization",
+                    AideBossApplication.getInstance().getTokenManager().accessToken.first()
+                ).build()
+        }
+        return proceed(accessToken)
     }
 }
