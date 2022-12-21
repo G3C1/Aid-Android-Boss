148d403a2724b24b75721323c5f001022e81a87a
khs3994 <z01039948003@gmail.com>
2022-12-21 10:54:29 +0900

    :memo: :: BaseUrl to FakeUrl

--- app/src/main/java/com/g3c1/aide/di/module/NetWorkModule.kt
+++ app/src/main/java/com/g3c1/aide/di/module/NetWorkModule.kt
@@ -1,6 +1,6 @@
 package com.g3c1.aide.di.module
 
-import com.g3c1.aide.di.utils.BaseUrl
+import com.g3c1.aide.di.utils.FakeUrl
 import com.g3c1.aide.remote.api.StoreAPI
 import com.g3c1.aide.remote.api.UserAPI
 import com.g3c1.aide.remote.utils.TokenInterceptor
@@ -36,7 +36,7 @@ object NetWorkModule {
         gsonConverterFactory: GsonConverterFactory
     ): Retrofit {
         return Retrofit.Builder()
-            .baseUrl(BaseUrl.BASE_URL)
+            .baseUrl(FakeUrl.BASE_URL)
             .client(okHttpClient)
             .client(provideOkhttpClient())
             .addConverterFactory(gsonConverterFactory)
