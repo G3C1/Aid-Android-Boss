1bfcafe03ceaea57b1727a83eadd60b5e2116952
khs3994 <z01039948003@gmail.com>
2022-12-28 20:12:58 +0900

    :sparkles: :: Api 생성

--- app/src/main/java/com/g3c1/aide/remote/api/StoreAPI.kt
+++ app/src/main/java/com/g3c1/aide/remote/api/StoreAPI.kt
@@ -1,10 +1,15 @@
 package com.g3c1.aide.remote.api
 
 import com.g3c1.aide.feature_store.data.dto.MyStoresInfoDTO
+import com.g3c1.aide.feature_store.data.dto.StoreInfoDTO
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface StoreAPI {
-    @GET("v2/store/")
+    @GET("v2/store")
     suspend fun getStoreInfoList(): Response<MyStoresInfoDTO>
+
+    @POST("v2/store")
+    suspend fun createStore(): Response<StoreInfoDTO>
 }
